715b1a700119ee1b7e902cc7ff7867eeb9713e07
PunithArtham <apunithnlg19@gmail.com>
2025-03-12 15:25:00 +0530

    authentication

--- server.js
+++ server.js
@@ -1,77 +1,42 @@
-var express = require('express');
-var mongoose = require('mongoose');
-
-var app = express();
-
+const express = require('express');
+const app = express();
 app.use(express.json());
-
-mongoose.connect("mongodb://127.0.0.1:27017/student")
-    .then(() => console.log("MongoDB connected successfully"))
-    .catch((err) => console.log("MongoDB connection error:", err));
-
-const studentSchema = new mongoose.Schema({
-
-
-    name: { type: String, required: true },
-    age: { type: Number },
-    city: { type: String }
+const jwt = require('jsonwebtoken');
+app.post('/student', (req, res)=>{
+    const {username, password} = req.body;
+    const token = jwt.sign({username, password} ,"cvr");
+    if(username == "punith" && password == "cvr123"){
+        res.status(200).json({token});
+    }
+    else{
+        res.status(400).json({message: "Invalid credentials"});
+    }
 });
 
-const Student = mongoose.model('Student', studentSchema);
-app.post('/student', (req, res) => {
-    const newStudent = new Student(req.body);
-
-    newStudent.save()
-        .then((student) => {
-            res.status(201).json({ message: 'Student created successfully', student });
-        })
-        .catch((err) => {
-            res.status(400).json({ message: 'Error creating student', error: err });
-        });
+app.get('/protected', authenticateToken, (req, res)=>{
+    res.status(200).json({"message": "Welocme to My Home Page"});
 });
 
-app.get('/getAll',(req,res)=>{
-    Student.find()
-    .then((student)=>{
-        res.status(201).json({message:"data",student});
-    })
-    .catch((err) => {
-        res.status(400).json({ message: 'Error creating student', error: err });
+
+app.get('/students' , (req, res)=>{
+    res.status(200).json({"message":"Here is a list of students"});
+});
+
+
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization'];
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, "cvr", (err, decoded)=>{
+        if(err){
+            return res.status(403).json({message: "Forbidden"});
+        }
+        else{
+            res.status(200).json({"message":decoded});
+            next();
+        }
     });
-});
-
-app.delete('/student/:name', (req, res) => {
-    const studentName = req.params.name;
-    
-    Student.findOneAndDelete({ name: studentName })
-        .then((deletedStudent) => {
-            if (deletedStudent) {
-                res.status(200).json({ message: 'Student deleted successfully', deletedStudent });
-            } else {
-                res.status(404).json({ message: 'Student not found' });
-            }
-        })
-        .catch((err) => {
-            res.status(400).json({ message: 'Error deleting student', error: err });
-        });
-});
-app.put('/student/update/:name', (req, res) => {
-    const studentName = req.params.name;
-    const updatedData = req.body;
-
-    Student.findOneAndUpdate({ name: studentName }, updatedData, { new: true })
-        .then((updatedStudent) => {
-            if (updatedStudent) {
-                res.status(200).json({ message: 'Student updated successfully', updatedStudent });
-            } else {
-                res.status(404).json({ message: 'Student not found' });
-            }
-        })
-        .catch((err) => {
-            res.status(400).json({ message: 'Error updating student', error: err });
-        });
-});
+}
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+  console.log('Server started at port 3000');
 });
